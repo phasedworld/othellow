98b6ff1ce73eced4d47e76c1d327390e7dd65299
Vito <98jiminlee@gmail.com>
2019-12-17 13:46:21 +0900

    dfsdfsdfsdfsdf

--- parkjieun.othellow/src/main/java/parkjieun/othellow/community/service/CommunityService.java
+++ parkjieun.othellow/src/main/java/parkjieun/othellow/community/service/CommunityService.java
@@ -3,7 +3,6 @@ package parkjieun.othellow.community.service;
 import java.util.List;
 
 import parkjieun.othellow.community.domain.Community;
-import parkjieun.othellow.community.domain.Paging;
 
 public interface CommunityService {
 	public List<Community> communityList();
@@ -18,7 +17,6 @@ public interface CommunityService {
 	// 게시물 총 갯수
 	public int countPost();
 	// 페이징 처리 게시글 조회
-	public List<Community> selectPost(Paging paging);
 	public List<Community> bestPost();
 	
 
